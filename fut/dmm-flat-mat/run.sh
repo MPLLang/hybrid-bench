d20339cc8d05380a555cb675812b62188f54d888
Sam Westrick <shwestrick@gmail.com>
2023-07-28 16:42:16 +0000

    run script

--- fut/dmm-flat-mat/run.sh
+++ fut/dmm-flat-mat/run.sh
@@ -1,7 +1,60 @@
 #!/bin/bash
+
+echo "cpu procs=[2,3,4,6,8,16,24,32]"
+for p in 2 3 4 6 8 16 24 32; do
+  ./test.mpl @mpl procs $p -- -n 8192 -warmup 5 -repeat 10 -impl cpu \
+  | grep average \
+  | tail -c+9 \
+  | head -c 6;
+  echo "";
+done
+
+echo "gpu"
+./test.mpl @mpl procs $p -- -n 8192 -warmup 5 -repeat 10 -impl gpu \
+| grep average \
+| tail -c+9 \
+| head -c 6;
+echo "";
+
+echo "hybrid K=8 procs=[2,3,4,6,8,16,24,32]"
+for p in 2 3 4 6 8 16 24 32; do
+  ./test.mpl @mpl procs $p -- -n 8192 -warmup 5 -repeat 10 -impl hybrid -sched-gpu-payout 8 \
+  | grep average \
+  | tail -c+9 \
+  | head -c 6;
+  echo "";
+done
+
+echo "hybrid K=4 procs=[2,3,4,6,8,16,24,32]"
+for p in 2 3 4 6 8 16 24 32; do
+  ./test.mpl @mpl procs $p -- -n 8192 -warmup 5 -repeat 10 -impl hybrid -sched-gpu-payout 4 \
+  | grep average \
+  | tail -c+9 \
+  | head -c 6;
+  echo "";
+done
+
+echo "hybrid K=2 procs=[2,3,4,6,8,16,24,32]"
+for p in 2 3 4 6 8 16 24 32; do
+  ./test.mpl @mpl procs $p -- -n 8192 -warmup 5 -repeat 10 -impl hybrid -sched-gpu-payout 2 \
+  | grep average \
+  | tail -c+9 \
+  | head -c 6;
+  echo "";
+done
+
+echo "hybrid K=1 procs=[2,3,4,6,8,16,24,32]"
+for p in 2 3 4 6 8 16 24 32; do
+  ./test.mpl @mpl procs $p -- -n 8192 -warmup 5 -repeat 10 -impl hybrid -sched-gpu-payout 1 \
+  | grep average \
+  | tail -c+9 \
+  | head -c 6;
+  echo "";
+done
+
 echo "hybrid K=0.5 procs=[2,3,4,6,8,16,24,32]"
 for p in 2 3 4 6 8 16 24 32; do
-  ./main.mpl @mpl procs $p -- -n 10000000 -warmup 3 -repeat 10 -impl hybrid -sched-gpu-payout 0.5 \
+  ./test.mpl @mpl procs $p -- -n 8192 -warmup 5 -repeat 10 -impl hybrid -sched-gpu-payout 0.5 \
   | grep average \
   | tail -c+9 \
   | head -c 6;
